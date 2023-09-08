e1e193ef59fbe6df06eb7cd593d5079833c4e105
EvelynOkpaleke <evelyn.okpaleke@gmail.com>
2023-09-07 17:06:34 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-  printf("Size of char: %zu byte(s)\n", sizeof(char));
-  printf("Size of short int: %zu byte(s)\n", sizeof(short int));
-  printf("Size of int: %zu byte(s)\n", sizeof(int));
-  printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-  printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
-  printf("Size of float: %zu byte(s)\n", sizeof(float));
-  printf("Size of double: %zu byte(s)\n", sizeof(double));
-  printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+  /* Declare variables of various types. */
+  char c;
+  short int si;
+  int i;
+  long int li;
+  long long int lli;
+  float f;
+  double d;
+  long double ld;
+
+  /* Print the size of each variable. */
+  printf("Size of char: %zu byte(s)\n", sizeof(c));
+  printf("Size of short int: %zu byte(s)\n", sizeof(si));
+  printf("Size of int: %zu byte(s)\n", sizeof(i));
+  printf("Size of long int: %zu byte(s)\n", sizeof(li));
+  printf("Size of long long int: %zu byte(s)\n", sizeof(lli));
+  printf("Size of float: %zu byte(s)\n", sizeof(f));
+  printf("Size of double: %zu byte(s)\n", sizeof(d));
+  printf("Size of long double: %zu byte(s)\n", sizeof(ld));
 
   return 0;
 }
